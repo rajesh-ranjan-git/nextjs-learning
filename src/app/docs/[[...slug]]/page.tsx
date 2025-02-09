bcc356f6a7276d0161ab1b50943a385b11a1f758
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-02-09 18:52:57 +0530

    slug fixed

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -1,20 +1,19 @@
-import { notFound } from "next/navigation";
-import { Metadata } from "next";
+"use client";
 
-export const metadata: Metadata = {
-  title: "Docs",
-};
+import { notFound, useRouter } from "next/navigation";
 
 const Docs = ({ params }: { params: { slug: String[] } }) => {
+  const router = useRouter();
+
   if (params.slug?.length === 1) {
     return (
-      <div className="flex justify-center items-center h-[62vh] text-5xl">
+      <div className="flex justify-center items-center h-[56vh] text-5xl">
         Viewing Docs for feature {params.slug[0]}
       </div>
     );
   } else if (params.slug?.length === 2) {
     return (
-      <div className="flex justify-center items-center h-[62vh] text-5xl">
+      <div className="flex justify-center items-center h-[56vh] text-5xl">
         Viewing Docs for feature {params.slug[0]} and concept {params.slug[1]}
       </div>
     );
@@ -22,9 +21,31 @@ const Docs = ({ params }: { params: { slug: String[] } }) => {
     notFound();
   }
 
+  const handleFeature = () => {
+    router.push("/docs/newFeature");
+  };
+
+  const handleConcept = () => {
+    router.push("/docs/newFeature/newConcept");
+  };
+
   return (
-    <div className="flex justify-center items-center h-[56vh] text-5xl">
-      Documentation Page
+    <div className="flex flex-col justify-center items-center gap-4 h-[56vh]">
+      <span className="text-5xl">Documentation Page</span>
+      <div className="flex justify-center items-center gap-4">
+        <button
+          className="hover:border-white bg-white hover:bg-black p-2 border rounded-lg text-black text-xl hover:text-white transition-all ease-in-out"
+          onClick={handleFeature}
+        >
+          New Feature
+        </button>
+        <button
+          className="hover:border-white bg-white hover:bg-black p-2 border rounded-lg text-black text-xl hover:text-white transition-all ease-in-out"
+          onClick={handleConcept}
+        >
+          New Concept
+        </button>
+      </div>
     </div>
   );
 };
